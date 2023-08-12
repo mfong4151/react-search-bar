dc8caa6fe0df053f8284cd198b9b08d4f3ee2e8e
Max <mfong415@gmail.com>
2023-08-11 23:40:16 -0700

    Finished trie

--- src/ds/trie.ts
+++ src/ds/trie.ts
@@ -6,6 +6,44 @@ export class Trie{
         this.root = new TrieNode();
     }
 
+    insert(word: string){
+        let curr: TrieNode = this.root;
+        
+        for(const c of word){
+            if(!curr.children[c]) curr.children[c] = new TrieNode(); 
+            curr = curr.children[c];
+        }
+        curr.end = true;
+        return
+    }
+    search(word: string){
+        let curr: TrieNode = this.root;
+        
+        for(const c of word){
+            if(!curr.children[c]) return false; 
+            curr = curr.children[c];
+        }
 
+        return curr.end;
+    }
 
+    startsWith(prefix: string){
+        let curr: TrieNode = this.root;
+        
+        for(const c of prefix){
+            if(!curr.children[c]) return false; 
+            curr = curr.children[c];
+        }
+
+        return true;
+    }
+
+    buildTrie(words: string){
+
+        for(const word of words){
+            this.insert(word)
+        }
+
+        return
+    }
 }
